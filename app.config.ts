82524f85431d078e6d1a5ee98c14c8c5b5997f77
Dax <daxnavarrete@gmail.com>
2026-05-17 20:07:30 -0500

    chore: set EAS owner

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,7 @@ import type { ExpoConfig } from 'expo/config';
 const config: ExpoConfig = {
   name: 'Cromo Swap',
   slug: 'cromo-swap-app',
+  owner: 'daxrpm',
   version: '0.1.0',
   orientation: 'portrait',
   icon: './assets/icon.png',
